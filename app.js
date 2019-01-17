1b3a0bbb63645b3cb2e0f6ef10bd5adad9653389
ptrompeter <patrick.trompeter@gmail.com>
2019-01-17 14:55:45 -0800

    Made a form and an event handler.

---
+++ app.js
@@ -0,0 +1,9 @@
+$( document).ready(function() {
+    const form1 = $("#display");
+
+    form1.submit(function( event ){
+        event.preventDefault();
+        $("#target").val("Got it!");
+        $("#target2").text("Target 2 grabbed.");
+    });
+});
